3d6789a7e9fde662c43c972e923df8e1c42df859
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-05-24 17:33:55 -0400

    fixed some mistakes I made and created button for login form

--- wmp/src/components/login.js
+++ wmp/src/components/login.js
@@ -2,8 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import "../App.css";
 
-function Login() {
-  return <h1>Testing Login</h1>;
+function Signup() {
+  return ( <form>
+      <div className='form'>
+        <h2>Login Credentials</h2>
+        <label>
+            Name:
+            <input
+                value=''
+                name='name'
+                type='text'
+            />
+        </label>
+        <label>
+            Password:
+            <input
+                value=''
+                name='password'
+                type='password'
+            />
+        </label>
+      </div>
+      <button id="submit-btn">Login</button>
+  </form>
+  )
 }
 
-export default Login;
+export default Signup;
--- wmp/src/components/signup.js
+++ wmp/src/components/signup.js
@@ -3,28 +3,7 @@ import { Route, Link, Switch } from "react-router-dom";
 import "../App.css";
 
 function Signup() {
-  return ( <form>
-      <div className='form'>
-        <h2>Login Credentials</h2>
-        <label>
-            Username:
-            <input
-                value=''
-                name='username'
-                type='text'
-            />
-        </label>
-        <label>
-            Password:
-            <input
-                value=''
-                name='password'
-                type='password'
-            />
-        </label>
-      </div>
-  </form>
-  )
+  return <h1>Testing Signup</h1>;
 }
 
 export default Signup;
