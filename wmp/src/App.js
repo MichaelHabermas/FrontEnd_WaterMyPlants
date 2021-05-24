e30ec6e0da0bd20ac4c63e3d85f8315c312d3947
Spencerp34 <spencerp34@yahoo.com>
2021-05-24 10:49:47 -0600

    set up home page and components

--- wmp/src/App.js
+++ wmp/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
 import './App.css';
+import Signup from './components/signup'
+import Login from './components/login'
+import Dashboard from './components/dashboard'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav class="nav main-nav">
+          <Link to='/'>Home</Link>
+          <Link to='/signup' id='signup' >Sign Up</Link>
+          <Link to='/login' id='login' >Log In</Link>
+        </nav>
       </header>
+
+      <Switch>
+        <Route path="/signup">
+          <Signup/>
+        </Route>
+        <Route path='/login'>
+          <Login/>
+        </Route>
+        <Route path='/' render={props => {
+          return (
+            <>
+              <h2>Learn to Water your plants properly!</h2>
+              <button>
+                <Link to='/login'>Log In!</Link>
+                <Link to='/signup'>Sign Up</Link>
+              </button>
+            </>
+          )
+        }}
+        />
+      </Switch>
     </div>
   );
 }
--- wmp/src/index.js
+++ wmp/src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Styled from 'styled-components'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Styled>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Styled>,
+  
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
