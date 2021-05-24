f28a5b9b6fd7ba9b0519de41de98f6a889a06b53
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-24 13:06:12 -0400

    actions and reducers

---
+++ wmp/src/reducers/index.js
@@ -0,0 +1,14 @@
+import {} from '../actions';
+
+const initialState = {};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case A:
+			return state;
+		default:
+			return state;
+	}
+};
+
+export default reducer;
