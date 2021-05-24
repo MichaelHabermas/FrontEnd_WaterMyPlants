c4820b45e81b741d014f039af329461bc813bc9c
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-24 16:06:10 -0400

    cleanup

--- wmp/src/App.js
+++ wmp/src/App.js
@@ -1,44 +1,49 @@
-import React from 'react'
-import { Route, Link, Switch } from 'react-router-dom'
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
-import Signup from './components/signup'
-import Login from './components/login'
-import Dashboard from './components/dashboard'
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<nav class="nav main-nav">
+					<Link to="/">Home</Link>
+					<Link to="/signup" id="signup">
+						Sign Up
+					</Link>
+					<Link to="/login" id="login">
+						Log In
+					</Link>
+				</nav>
+			</header>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <nav class="nav main-nav">
-          <Link to='/'>Home</Link>
-          <Link to='/signup' id='signup' >Sign Up</Link>
-          <Link to='/login' id='login' >Log In</Link>
-        </nav>
-      </header>
-
-      <Switch>
-        <Route path="/signup">
-          <Signup/>
-        </Route>
-        <Route path='/login'>
-          <Login/>
-        </Route>
-        <Route path='/' render={props => {
-          return (
-            <>
-              <h2>Learn to Water your plants properly!</h2>
-              <button>
-                <Link to='/login'>Log In!</Link>
-                <Link to='/signup'>Sign Up</Link>
-              </button>
-            </>
-          )
-        }}
-        />
-      </Switch>
-    </div>
-  );
+			<Switch>
+				<Route path="/signup">
+					<Signup />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route
+					path="/"
+					render={props => {
+						return (
+							<>
+								<h2>Learn to Water your plants properly!</h2>
+								<button>
+									<Link to="/login">Log In!</Link>
+									<Link to="/signup">Sign Up</Link>
+								</button>
+							</>
+						);
+					}}
+				/>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
