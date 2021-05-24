8181724a70a7b672e847e356ffa031da84ca9682
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-24 15:09:05 -0400

    newest

--- wmp/src/reducers/index.js
+++ wmp/src/reducers/index.js
@@ -1,11 +1,31 @@
-import {} from '../actions';
+import plantData from '../plantData';
+import { FETCH_START, FETCH_SUCCESS, FETCH_FAIL } from '../actions';
 
-const initialState = {};
+const initialState = {
+	plantData: [...plantData],
+	isFetching: false,
+	error: ''
+};
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case A:
-			return state;
+		case FETCH_START:
+			return {
+				...state,
+				isFetching: true
+			};
+		case FETCH_SUCCESS:
+			return {
+				...state,
+				isFetching: false,
+				error: ''
+			};
+		case FETCH_FAIL:
+			return {
+				...state,
+				isFetching: false,
+				error: action.payload
+			};
 		default:
 			return state;
 	}
