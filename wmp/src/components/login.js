3442fed5747e047a74e3ef674f1ec6d832ac78ce
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-25 09:11:41 -0400

    file changes

--- wmp/src/components/login.js
+++ wmp/src/components/login.js
@@ -1,19 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { Route, Link, Switch } from "react-router-dom";
-import "../App.css";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import '../App.css';
 
+import { logIn } from '../actions/index';
 
-
-
+const initialCredentials = {
+	username: '',
+	password: ''
+};
 
 function Login() {
+	const [credentials, setCredentials] = useState(initialCredentials);
 
+	const history = useHistory();
 
+	const handleChange = e => {
+		setCredentials({
+			...credentials,
+			[e.target.name]: e.target.value
+		});
+	};
 
+	const submitLogin = e => {
+		e.preventDefault();
+		logIn(credentials); // this will need updating RE the PUT call
+		history.push('/dashboard');
+	};
 
-
-
-  return <h1>Testing Login</h1>;
+	return <h1>Testing Login</h1>;
 }
 
 export default Login;
+// export default connect(null, {})(Login);
