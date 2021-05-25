eaa864e678b04055e18cc03238224c774467750a
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-05-25 09:16:40 -0400

    had to rewrite the login form

--- wmp/src/components/login.js
+++ wmp/src/components/login.js
@@ -7,13 +7,38 @@ import "../App.css";
 
 
 function Login() {
+  return (
+      <div className='login-container'>
+           <div className='Form'>
+               <h2>Sign up for Water My Plants Today!</h2>
+               <form id='login'>
+                   <label> Name:
+                   <input 
+                       type='text'
+                       name='name'
+                       id='name-input'
+                   />
+                   </label>
+                   <br/>
+                   <label> Password:
+                       <input 
+                           type='password'
+                           name='password'
+                           id='password-input'
+                           minLength='5'
+                       />
+                   </label>
+                   <br/>
+                   <div className='submit'>
+                       <button id='login-btn'>Login!</button>
+                   </div>
 
+               </form>
+           </div>
+      </div>
+   
+   )
 
-
-
-
-
-  return <h1>Testing Login</h1>;
 }
 
 export default Login;
