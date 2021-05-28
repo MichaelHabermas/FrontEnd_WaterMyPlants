561c6c99916b438224038dfbb7e73586b0beb06b
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-28 08:48:33 -0400

    setup

--- wmp/src/components/Account.js
+++ wmp/src/components/Account.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { connect } from 'react-redux';
 
-function Account() {
+const initialAccount = {
+	username: '',
+	password: ''
+};
+
+function Account(props) {
+	const [account, setAccount] = useState(initialAccount);
+	const { dispatch } = props;
+
+	const handleChange = e => {
+		setNewPlant({
+			...newPlant,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		axiosWithAuth()
+			.post(`https://ft-water-my-plants-3.herokuapp.com/api/plants/user/${props.userId}`, newPlant)
+			.then(res => {
+				dispatch(addPlant(newPlant));
+				setNewPlant(initialPlant);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	};
+
 	return (
-		<div>
-			<h1>Account Page</h1>
+		<div className="newPlant">
+			<div className="builder">
+				<h2>Add a new Plant!</h2>
+				<form onSubmit={handleSubmit} id="plant-form">
+					<label>
+						{' '}
+						Species
+						<input
+							type="text"
+							name="species"
+							id="species-input"
+							value={newPlant.species}
+							onChange={handleChange}
+						/>
+					</label>
+					<label>
+						{' '}
+						Plant's Nickname
+						<input
+							type="text"
+							name="nickname"
+							id="nickname-input"
+							value={newPlant.nickname}
+							onChange={handleChange}
+						/>
+					</label>
+					<label>
+						{' '}
+						Water Frequency
+						<input
+							type="text"
+							name="h2o_frequency"
+							id="water-input"
+							value={newPlant.h2o_frequency}
+							onChange={handleChange}
+						/>
+					</label>
+					<div className="submit">
+						<button id="newPlant-button">Add New Plant!</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 }
